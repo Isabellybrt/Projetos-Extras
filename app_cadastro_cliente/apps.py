ccf6713a80c7d63631af2995b8460248f4445803
Maria Isabelly de Brito Rodrigues <118930305+Isabellybrt@users.noreply.github.com>
2024-04-18 13:23:40 -0300

    Delete app_cadastro_cliente directory

--- app_cadastro_cliente/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCadastroClienteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_cadastro_cliente'
--- app_cadastro_cliente/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Cliente(models.Model):
-    cpf = models.TextField(primary_key=True, max_length=15)
-    nome = models.TextField(max_length=255)
-    rua = models.TextField(max_length=255)
-    cidade = models.TextField(max_length=60)
-    numero = models.TextField(max_length=5)
-    estado = models.TextField(max_length=20)
